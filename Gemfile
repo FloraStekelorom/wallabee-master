3766142ccb39adba50a934fcfeea2701fdf087c3
Flora <flora.e.hewitt@gmail.com>
2021-01-27 20:19:50 +0400

    added font awesome

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+#Use fontawesome
+gem "font-awesome-rails"
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 
+import './layout.scss';
+
 const Layout = (props) => {
   return (
     <React.Fragment>
-      <nav className="navbar navbar-expand navbar-light bg-light">
-        <a href="/"><span className="navbar-brand mb-0 h1 text-danger">Wallabee</span></a>
+
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="navbar-header">
+          <a href="/"><span className="navbar-brand mb-0 h1 text-light">Wallabee</span></a>
+        </div>
         <div className="collapse navbar-collapse">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
+          <ul className="navbar-nav ml-auto">
+            <li className="dropdown">
+              <a href="#" className="dropdown-toggle text-light btn-lg" data-toggle="dropdown" role="button" aria-expanded="false"><i className="fa fa-user-circle-o text-light" aria-hidden="true"></i></a>
+              <ul className="dropdown-menu row dropdown-menu-right" role="menu">
+                <li ><a href="#" className="text-secondary username mx-3" >Log out</a></li>
+                <div className="dropdown-divider"></div>
+                <li ><a href="/" className="text-secondary mx-3">Your products</a></li>
+                <div className="dropdown-divider"></div>
+                <li ><a href="/" className="text-secondary mx-3">Your Rentals</a></li>
+              </ul>
             </li>
           </ul>
         </div>
       </nav>
       {props.children}
-      <footer className="p-3 bg-light">
+      <footer className="p-3 bg-dark">
         <div>
-          <p className="mr-3 mb-0 text-secondary">Wallabee</p>
+          <p className="mr-3 mb-0 text-light">&#169; 2021 Wallabee. All Rights Reserved</p>
         </div>
       </footer>
     </React.Fragment>
