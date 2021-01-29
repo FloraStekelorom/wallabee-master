434013c39cfafba17619eab177692aaad52bb2bf
Flora <flora.e.hewitt@gmail.com>
2021-01-29 10:28:29 +0400

    added footer

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -5,7 +5,6 @@ import './layout.scss';
 const Layout = (props) => {
   return (
     <React.Fragment>
-
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <div className="navbar-header">
           <a href="/"><span className="navbar-brand mb-0 h1 text-light">Wallabee</span></a>
@@ -15,7 +14,7 @@ const Layout = (props) => {
             <li className="dropdown">
               <a href="#" className="dropdown-toggle text-light btn-lg" data-toggle="dropdown" role="button" aria-expanded="false"><i className="fa fa-user-circle-o text-light" aria-hidden="true"></i></a>
               <ul className="dropdown-menu row dropdown-menu-right" role="menu">
-                <li ><a href="#" className="text-secondary username mx-3" >Log out</a></li>
+                <li ><a href="#" className="text-secondary mx-3" >Log out</a></li>
                 <div className="dropdown-divider"></div>
                 <li ><a href="/" className="text-secondary mx-3">Your products</a></li>
                 <div className="dropdown-divider"></div>
@@ -25,11 +24,21 @@ const Layout = (props) => {
           </ul>
         </div>
       </nav>
+
       {props.children}
-      <footer className="p-3 bg-dark">
+
+      <footer className="py-1 px-3 navbar navbar-expand navbar-dark bg-dark" role="navigation">
         <div>
-          <p className="mr-3 mb-0 text-light">&#169; 2021 Wallabee. All Rights Reserved</p>
+          <p className="mr-3 mb-0 text-light footer-text">&#169; 2021 Wallabee. All Rights Reserved</p>
         </div>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <a href="#" className="btn-lg"><i className="fa fa-facebook footer-menu"></i></a>
+            <a href="#" className="btn-lg"><i className="fa fa-twitter footer-menu"></i></a>
+            <a href="#" className="btn-lg"><i className="fa fa-instagram footer-menu"></i></a>
+          </ul>
+        </div>
+
       </footer>
     </React.Fragment>
   );
