79aebb484f16ddb23870cb6ec0a1694cba082aea
Flora <flora.e.hewitt@gmail.com>
2021-01-29 18:57:49 +0400

    fixed footer at the bottom of the page

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -41,40 +41,45 @@ class Layout extends React.Component {
 
       return (
         <React.Fragment>
-          <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <div className="navbar-header">
-              <a href="/"><span className="navbar-brand mb-0 h1 text-light">Wallabee</span></a>
-            </div>
-            <div className="collapse navbar-collapse">
-              <ul className="navbar-nav ml-auto">
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle text-light btn-lg" data-toggle="dropdown" role="button" aria-expanded="false"><i className="fa fa-user-circle-o text-light" aria-hidden="true"></i></a>
-                  <ul className="dropdown-menu row dropdown-menu-right" role="menu">
-                    {authenticated ? <li ><a href="#" className="text-secondary username mx-3" onClick={this.logout}>Log out</a></li> : <li ><a href={`/login`} className="text-secondary mx-3">Log in</a></li>}
-                    <div className="dropdown-divider"></div>
-                    <li ><a href="/" className="text-secondary mx-3">Your products</a></li>
-                    <div className="dropdown-divider"></div>
-                    <li ><a href="/" className="text-secondary mx-3">Your Rentals</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </nav>
+          <div className="d-flex flex-column min-vh-100">
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+              <div className="navbar-header">
+                <a href="/"><span className="navbar-brand mb-0 h1 text-light">Wallabee</span></a>
+              </div>
+              <div className="collapse navbar-collapse">
+                <ul className="navbar-nav ml-auto">
+                  <li className="dropdown">
+                    <a href="#" className="dropdown-toggle text-light btn-lg" data-toggle="dropdown" role="button" aria-expanded="false"><i className="fa fa-user-circle-o text-light" aria-hidden="true"></i></a>
+                    <ul className="dropdown-menu row dropdown-menu-right" role="menu">
+                      {authenticated ? <li ><a href="#" className="text-secondary username mx-3" onClick={this.logout}>Log out</a></li> : <li ><a href={`/login`} className="text-secondary mx-3">Log in</a></li>}
+                      <div className="dropdown-divider"></div>
+                      <li ><a href="/" className="text-secondary mx-3">Your products</a></li>
+                      <div className="dropdown-divider"></div>
+                      <li ><a href="/" className="text-secondary mx-3">Your Rentals</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </nav>
 
-          {this.props.children}
+            <main className="flex-fill">
+              {this.props.children}
+            </main>
 
-          <footer className="py-1 px-3 navbar navbar-expand navbar-dark bg-dark" role="navigation">
-            <div>
-              <p className="mr-3 mb-0 text-light footer-text">&#169; 2021 Wallabee. All Rights Reserved</p>
-            </div>
-            <div className="collapse navbar-collapse">
-              <ul className="navbar-nav ml-auto">
-                <a href="#" className="btn-lg"><i className="fa fa-facebook footer-menu"></i></a>
-                <a href="#" className="btn-lg"><i className="fa fa-twitter footer-menu"></i></a>
-                <a href="#" className="btn-lg"><i className="fa fa-instagram footer-menu"></i></a>
-              </ul>
-            </div>
-          </footer>
+            <footer className="footer mt-auto pt-1 px-3 navbar navbar-expand navbar-dark bg-dark" role="navigation">
+              <div>
+                <p className="mr-3 mb-0 text-light footer-text">&#169; 2021 Wallabee. All Rights Reserved</p>
+              </div>
+              <div className="collapse navbar-collapse">
+                <ul className="navbar-nav ml-auto">
+                  <a href="#" className="btn-lg"><i className="fa fa-facebook footer-menu"></i></a>
+                  <a href="#" className="btn-lg"><i className="fa fa-twitter footer-menu"></i></a>
+                  <a href="#" className="btn-lg"><i className="fa fa-instagram footer-menu"></i></a>
+                </ul>
+              </div>
+            </footer>
+          </div>
+
         </React.Fragment>
       );
     }
